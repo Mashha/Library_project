6cab3f7e00f8d1585e88a4c817b71844f6c40adc
Masa Orel <orel.masha89@gmail.com>
2022-10-05 16:01:53 +0200

    added blur effect to the background

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ let main = document.querySelector('.main-inner')
 function addBookToTable() {
   //card with books
   let bookCard = document.createElement('div')
-  let bookCardLeft = document.createElement("div")
-  let bookCardRight = document.createElement("div")
+  let bookCardLeft = document.createElement('div')
+  let bookCardRight = document.createElement('div')
 
   // main, left and right part
   main.appendChild(bookCard)
@@ -25,8 +25,8 @@ function addBookToTable() {
   bookCard.appendChild(bookCardRight)
 
   bookCard.classList.add('card')
-  bookCardLeft.classList.add("card-left")
-  bookCardRight.classList.add("card-right")
+  bookCardLeft.classList.add('card-left')
+  bookCardRight.classList.add('card-right')
   bookCard.id = `${Date.now()}`
 
   let removeBook = document.createElement('button')
@@ -34,16 +34,15 @@ function addBookToTable() {
   let cardAuthor = document.createElement('h4')
   let cardPages = document.createElement('p')
   let cardHaveRead = document.createElement('span')
-  let cardBookImg = document.createElement("img")
+  let cardBookImg = document.createElement('img')
 
-  cardBookImg.classList.add("imageOfBook")
-  
+  cardBookImg.classList.add('imageOfBook')
+
   cardTitle.textContent = title.value
   cardAuthor.textContent = author.value
   cardPages.textContent = `${pages.value} ${'pages'}`
   removeBook.classList.add('fa-regular', 'fa-trash-can')
 
-  
   bookCardRight.appendChild(removeBook)
   bookCardRight.appendChild(cardTitle)
   bookCardRight.appendChild(cardAuthor)
@@ -51,15 +50,13 @@ function addBookToTable() {
   bookCardRight.appendChild(cardHaveRead)
   bookCardLeft.appendChild(cardBookImg)
 
-
-  //add image 
-  let imageBookForCard = document.getElementById("imgUrl").value
-  if(imageBookForCard === "") {
-    cardBookImg.setAttribute("src", "images/mock_cover.svg")
+  //add image
+  let imageBookForCard = document.getElementById('imgUrl').value
+  if (imageBookForCard === '') {
+    cardBookImg.setAttribute('src', 'images/mock_cover.svg')
   } else {
-    cardBookImg.setAttribute("src", imageBookForCard)
+    cardBookImg.setAttribute('src', imageBookForCard)
   }
-  
 
   //add icon to haveRead
   let checkBox = document.getElementById('haveRead')
@@ -90,7 +87,9 @@ function addBookToTable() {
 
   //remove books from library and table one by one
   removeBook.addEventListener('click', function (bookCard) {
-    myLibrary = myLibrary.filter((bookCard) => bookCard.id !== this.parentElement.parentElement.id)
+    myLibrary = myLibrary.filter(
+      (bookCard) => bookCard.id !== this.parentElement.parentElement.id,
+    )
     if (bookCard.id === Object.id) {
       this.parentElement.parentElement.remove()
     }
@@ -107,7 +106,7 @@ document.querySelector('form').addEventListener('submit', function (e) {
   let authorValue = document.getElementById('author').value
   let pagesNum = document.getElementById('pages').value
   let haveReadBook = document.getElementById('haveRead').checked
-  let bookUrl = document.getElementById("imgUrl").value
+  let bookUrl = document.getElementById('imgUrl').value
 
   let idValue = `${Date.now()}`
   //add new book
@@ -125,8 +124,9 @@ document.querySelector('form').addEventListener('submit', function (e) {
   // push a book to array
   myLibrary.push(newBook)
 
-  //close the modal
+  //close the modal and remove the blur effect
   modal.classList.toggle('active')
+  wrapper.classList.toggle('blur')
 
   //clear the input fields
   document.querySelectorAll('input').forEach((node) => {
@@ -136,12 +136,14 @@ document.querySelector('form').addEventListener('submit', function (e) {
 
 //open/close the modal
 let modal = document.querySelector('.modal-form')
+let wrapper = document.querySelector('.wrapper')
 
 document.querySelector('.addNewBook').addEventListener('click', showModalForm)
 document.querySelector('.close').addEventListener('click', showModalForm)
 
 function showModalForm() {
   modal.classList.toggle('active')
+  wrapper.classList.toggle('blur')
 }
 
 //clear the table and array
